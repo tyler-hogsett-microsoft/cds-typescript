4da648d57304fc8ca9b5c5240fd7a424ee4d17cb
Tyler Hogsett <tyhogset@microsoft.com>
2020-06-25 10:01:27 -0700

    set up prettier and ran it on test.ts

--- test.ts
+++ test.ts
@@ -3,20 +3,20 @@ import { expect } from "chai";
 var assert = require("assert");
 
 describe("Array", () => {
-    describe(".indexOf()", () => {
-        it("returns -1 when the given value does not exist", () => {
-            const result = [1, 2, 3].indexOf(4);
-            expect(result).to.equal(-1);
-        });
-
-        it("returns the correct index when the given value does exist", () => {
-            const result = [1, 2, 3].indexOf(1)
-            expect(result).to.equal(0);
-        });
-
-        it("returns the first index of the given value if the value exists in multiple locations", () => {
-            var result = [1, 1, 1].indexOf(1);
-            expect(result).to.equal(0);
-        });
+  describe(".indexOf()", () => {
+    it("returns -1 when the given value does not exist", () => {
+      const result = [1, 2, 3].indexOf(4);
+      expect(result).to.equal(-1);
     });
+
+    it("returns the correct index when the given value does exist", () => {
+      const result = [1, 2, 3].indexOf(1);
+      expect(result).to.equal(0);
+    });
+
+    it("returns the first index of the given value if the value exists in multiple locations", () => {
+      var result = [1, 1, 1].indexOf(1);
+      expect(result).to.equal(0);
+    });
+  });
 });
