7087b093299dadb07ec4a80cecaf806dffd9c2b9
Tyler Hogsett <tyhogset@microsoft.com>
2020-06-25 09:39:00 -0700

    set up watch mode for ts-mocha

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 var assert = require("assert");
 
 describe("Array", () => {
-    describe("#indexOf()", () => {
+    describe(".indexOf()", () => {
         it("returns -1 when the given value does not exist", () => {
             assert.equal(
                 [1, 2, 3].indexOf(4),
@@ -9,9 +9,16 @@ describe("Array", () => {
             );
         });
 
-        it("return the correct index when the given value does exist", () => {
+        it("returns the correct index when the given value does exist", () => {
             assert.equal(
                 [1, 2, 3].indexOf(1),
+                0
+            );
+        });
+
+        it("returns the first index of the given value if the value exists in multiple locations", () => {
+            assert.equal(
+                [1, 1, 1].indexOf(1),
                 0
             );
         });
