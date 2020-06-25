409165d38083611278367af2a7c0f0626ad21388
Tyler Hogsett <tyhogset@microsoft.com>
2020-06-25 09:51:16 -0700

    set up chai

--- test.ts
+++ test.ts
@@ -1,26 +1,22 @@
+import { expect } from "chai";
+
 var assert = require("assert");
 
 describe("Array", () => {
     describe(".indexOf()", () => {
         it("returns -1 when the given value does not exist", () => {
-            assert.equal(
-                [1, 2, 3].indexOf(4),
-                -1
-            );
+            const result = [1, 2, 3].indexOf(4);
+            expect(result).to.equal(-1);
         });
 
         it("returns the correct index when the given value does exist", () => {
-            assert.equal(
-                [1, 2, 3].indexOf(1),
-                0
-            );
+            const result = [1, 2, 3].indexOf(1)
+            expect(result).to.equal(0);
         });
 
         it("returns the first index of the given value if the value exists in multiple locations", () => {
-            assert.equal(
-                [1, 1, 1].indexOf(1),
-                0
-            );
+            var result = [1, 1, 1].indexOf(1);
+            expect(result).to.equal(0);
         });
     });
 });
